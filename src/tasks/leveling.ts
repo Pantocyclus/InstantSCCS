151ed0b66e570da75cffdf4e362ac747a3fd5b4a
Pantocyclus <98746573+Pantocyclus@users.noreply.github.com>
2023-03-15 16:21:03 +0800

    URLs are different depending on whether we have Shadow Affinity

--- src/tasks/leveling.ts
+++ src/tasks/leveling.ts
@@ -430,7 +430,9 @@ export const LevelingQuest: Quest = {
       // eslint-disable-next-line libram/verify-constants
       do: (): void => {
         const target = get("rufusQuestTarget", "");
-        visitUrl("place.php?whichplace=town_right&action=townright_shadowrift_free");
+        if (have($effect`Shadow Affinity))
+          visitUrl("place.php?whichplace=town_right&action=townright_shadowrift_free");
+        else visitUrl("place.php?whichplace=town_right&action=townright_shadowrift");
         if (lastChoice() === 1499) {
           let NCChoice = 6;
           let tries = 0;
