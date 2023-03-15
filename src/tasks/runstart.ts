fd0f8504f962f290609af42915c2255de7b1358e
Pantocyclus <98746573+Pantocyclus@users.noreply.github.com>
2023-03-15 10:10:56 +0800

    Don't buy yellow rocket if we have ELY

--- src/tasks/runstart.ts
+++ src/tasks/runstart.ts
@@ -310,7 +310,8 @@ export const RunStartQuest: Quest = {
       name: "Novelty Tropical Skeleton",
       ready: () => !have($effect`Everything Looks Yellow`) || have($item`cherry`),
       prepare: (): void => {
-        if (!have($item`yellow rocket`)) buy($item`yellow rocket`, 1);
+        if (!have($item`yellow rocket`) && !(have($effect`Everything Looks Yellow`)))
+          buy($item`yellow rocket`, 1);
         if (have($item`unbreakable umbrella`) && get("umbrellaState") !== "broken")
           cliExecute("umbrella ml");
         if (get("_snokebombUsed") === 0) restoreMp(50);
