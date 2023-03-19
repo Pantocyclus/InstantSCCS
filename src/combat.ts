e24c173f63f29b30853bc7400a3af84588146e16
Pantocyclus <98746573+Pantocyclus@users.noreply.github.com>
2023-03-19 14:53:51 +0800

    Exit while loop if we don't have the skill

--- src/combat.ts
+++ src/combat.ts
@@ -1,17 +1,23 @@
-import { mpCost, myClass } from "kolmafia";
+import { mpCost, myClass, toInt } from "kolmafia";
 import { $skill, StrictMacro } from "libram";
 
 export const mainStat = myClass().primestat;
 
 export default class Macro extends StrictMacro {
   kill(): Macro {
-    return this.skill($skill`Curse of Weaksauce`)
+    return this.trySkill($skill`Curse of Weaksauce`)
       .if_(
         `!mpbelow ${mpCost($skill`Stuffed Mortar Shell`)}`,
         Macro.trySkill($skill`Stuffed Mortar Shell`)
       )
-      .while_(`!mpbelow ${mpCost($skill`Saucegeyser`)}`, Macro.skill($skill`Saucegeyser`))
-      .while_(`!mpbelow ${mpCost($skill`Saucestorm`)}`, Macro.trySkill($skill`Saucestorm`))
+      .while_(
+        `!mpbelow ${mpCost($skill`Saucegeyser`)} && hasskill ${toInt($skill`Saucegeyser`)}`,
+        Macro.skill($skill`Saucegeyser`)
+      )
+      .while_(
+        `!mpbelow ${mpCost($skill`Saucestorm`)} && hasskill ${toInt($skill`Saucestorm`)}`,
+        Macro.skill($skill`Saucestorm`)
+      )
       .attack()
       .repeat();
   }
