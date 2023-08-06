e53e9c3fdcb43b735e7f4e603df92882bafb0ea1
Pantocyclus <98746573+Pantocyclus@users.noreply.github.com>
2023-08-06 10:44:49 +0800

    Require mafia version 27518

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,7 @@ export const args = Args.create("InstantSCCS", "An automated low-shiny SCCS scri
 });
 
 export function main(command?: string): void {
-  sinceKolmafiaRevision(27507);
+  sinceKolmafiaRevision(27518);
   checkGithubVersion();
 
   Args.fill(args, command);
