cd6ff5985ead725088888c510dbe7a4faadbf2b1
Pantocyclus <98746573+Pantocyclus@users.noreply.github.com>
2026-03-04 07:17:00 +0800

    Remove unnecessary aborts

--- src/tasks/leveling.ts
+++ src/tasks/leveling.ts
@@ -1442,7 +1442,7 @@ export const LevelingQuest: Quest = {
           .externalIf(
             get("_clubEmNextWeekUsed", 0) < 5 - get("instant_saveClubEmNextWeek", 0),
             // eslint-disable-next-line libram/verify-constants
-            Macro.trySkill($skill`Club 'Em Into Next Week`).abort(),
+            Macro.trySkill($skill`Club 'Em Into Next Week`),
           )
           .default(useCinch),
       ),
@@ -1771,7 +1771,7 @@ export const LevelingQuest: Quest = {
           .externalIf(
             get("_clubEmNextWeekUsed", 0) < 5 - get("instant_saveClubEmNextWeek", 0),
             // eslint-disable-next-line libram/verify-constants
-            Macro.trySkill($skill`Club 'Em Into Next Week`).abort(),
+            Macro.trySkill($skill`Club 'Em Into Next Week`),
           )
           .default(useCinch),
       ),
@@ -1954,7 +1954,7 @@ export const LevelingQuest: Quest = {
           .externalIf(
             get("_clubEmNextWeekUsed", 0) < 5 - get("instant_saveClubEmNextWeek", 0),
             // eslint-disable-next-line libram/verify-constants
-            Macro.trySkill($skill`Club 'Em Into Next Week`).abort(),
+            Macro.trySkill($skill`Club 'Em Into Next Week`),
           )
           .default(useCinch),
       ),
